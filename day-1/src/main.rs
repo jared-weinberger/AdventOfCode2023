a3b127f644a80863bfc3a5eab8cc4925942249dd
Jared <jared.j.weinberger@gmail.com>
2023-12-11 23:34:29 -0700

    Removed aborted solution

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -96,27 +96,6 @@ impl CalibrationParser {
         numbers
     }
 
-    const DIGIT_TABLE: [(&'static str, u32); 18] = [
-        ("1", 1),
-        ("2", 2),
-        ("3", 3),
-        ("4", 4),
-        ("5", 5),
-        ("6", 6),
-        ("7", 7),
-        ("8", 8),
-        ("9", 9),
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ];
-
     fn parse_word_digit(text: &str) -> Option<u32> {
         match text {
             "one" => Some(1),
